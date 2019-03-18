82cfd2a71eb200f83bef664a28de5599a3f77dda
alex <alexandre.gracias@coding-academy.fr>
2019-03-18 16:43:18 +0100

    footer

--- Register/inc/footer.php
+++ Register/inc/footer.php
@@ -1,25 +1,16 @@
+<?php include_once 'inc/header.php' ?>
+
 <br>
 <br>
-<div class="card">
-    <div class="card-header">
-    </div>
- <div class="card-body">
- <div class="col-md-15 text-left">
- <a href="https://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://www.example.com&p[images][0]=&p[title]=Title%20Goes%20Here&p[summary]=Description%20goes%20here!" 
- style="width:20%; margin-top:10px; type="button" title="Facebook" 
- class="btn btn-facebook btn-lg" class="btn btn-primary center-block"><i class="fa fa-facebook fa-fw"><i class="fa fa-facebook fa-2"></i> Share on Facebook</button>
- <a href="https://twitter.com/share?url=http://www.bootstrapbeginners.com/twitter-button/"  style="width:20%; margin-top:10px; type="button" title="Twitter" 
- class="btn btn-twitter btn-lg" class="btn btn-primary center-block"><i class="fa fa-twitter fa-fw"></i> Share on Twitter</a>
-</a>
 
-    
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<script src="https://cdn.jsdelivr.net/sharer.js/latest/sharer.min.js"></script>
 
-
-
-
-
-
+<div class="social float-left">
+<a href="https://www.twitter.com" id="share-tw" class="sharer button"><i class="fa fa-3x fa-twitter-square"></i></a>
+<a href="https://www.facebook.com" id="share-fb" class="sharer button"><i class="fa fa-3x fa-facebook-square"></i></a>
 </div>
+
 </body>
 </html>
 
