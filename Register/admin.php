5706b1be7564e71b74d3e33a48aea635e5fcd0ca
Jerome <jerome.bonnier@coding-academy.fr>
2019-03-18 15:46:01 +0100

    Modif admin.php

--- Register/admin.php
+++ Register/admin.php
@@ -4,7 +4,6 @@ session_start();
 include_once 'inc/function.php';
 include_once 'inc/header.php';
 include_once 'inc/db.php';
-include_once 'inc/footer.php';
 ?>
 
 <?php
@@ -24,3 +23,57 @@ else{
     header('Location: index.php');
 //    echo "toto";
 } ?>
+
+
+
+<div class="container">
+
+<table class="table table-striped table-bordered table-hover">   
+<thead class="thead-dark">
+    <tr>
+      <th scope="col">Nom</th>
+      <th scope="col">Email</th>
+      <th scope="col">Edit User</th>
+      <th scope="col">Delete User</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <?php
+        $connect = $pdo->prepare("SELECT username, email FROM users ORDER BY username");
+        $connect->execute();
+        $table = $connect->fetchAll();
+
+        //print_r($table);?>
+
+        
+        <?php
+    
+        foreach($table as $object)
+        {?>
+        
+        <tr>
+        <?php
+        foreach($object as $values)
+            {?>
+                <td><?php echo $values; ?></td>
+            <?php
+            }?>
+                <td>button edit</td>
+                <td>button delete</td>
+            </tr>
+            <?php
+        }?>
+
+</tbody>
+</table>
+</div>
+
+
+
+
+
+<?php
+include_once 'inc/footer.php';
+?>
+    
