2b6b5462a760d821c6d6b9111e3c69194f51f1b9
Thibaud <thibaud.levy@coding-academy.fr>
2019-03-19 10:33:47 +0100

    ajout d'une fonction pour delete un utilisateur

--- Register/inc/function.php
+++ Register/inc/function.php
@@ -8,5 +8,11 @@ function str_random($length){
     $alphabet = "0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
     return substr(str_shuffle(str_repeat($alphabet,$length)), 0, $length);
 
+}
+
+function deleteUser($email){
+
+	include_once "db.php";
+	return $pdo->query("DELETE FROM users WHERE email ='$email' ");
 
 }
---
+++ Register/user_page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: titi
+ * Date: 19/03/19
+ * Time: 10:07
+ */
