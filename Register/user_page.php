43ae10c3631247c8d8b907bbbeb59ad3896ce33b
Thibaud <thibaud.levy@coding-academy.fr>
2019-03-19 13:23:06 +0100

    ajout de la page de display de l'utilisateur

--- Register/user_page.php
+++ Register/user_page.php
@@ -1,4 +1,53 @@
 <?php
-include_once "inc/header.php";
+session_start();
+
+include_once 'inc/function.php';
+include_once 'inc/header.php';
+include_once 'inc/db.php';
 ?>
 
+<?php
+
+$email = $_SESSION['auth']->email;
+$query = $pdo->query("SELECT * FROM users WHERE email = '$email'");
+$admin = $query->fetch(PDO::FETCH_OBJ);
+
+if(($_SESSION["auth"]->admin == 1)){
+
+	$id = $_GET['user_id'];
+	$req = $pdo->Prepare("SELECT * FROM users WHERE id = '$id'");
+	$req->execute();
+	$user = $req->fetch();
+	if($user->admin == 1){
+		$user->admin = "Administrateur";
+	}
+	else{
+		$user->admin = "Utilisateur";
+	}
+}
+else{
+	header('Location: index.php');
+	//    echo "toto";
+}
+
+?>
+<div class="container">
+	<h2>Hover Rows</h2>
+	<p></p>
+	<table class="table table-hover">
+		<thead>
+		<tr>
+			<th>Firstname</th>
+			<th>Email</th>
+			<th>Statut</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td><?= $user->username ?></td>
+			<td><?= $user->email?></td>
+			<td><?= $user->admin ?></td>
+		</tr>
+		</tbody>
+	</table>
+</div>
