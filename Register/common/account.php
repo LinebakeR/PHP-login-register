aec8f3731dc4573877e9bd78ea2f339761e5b5d4
Thibaud <thibaud.levy@coding-academy.fr>
2019-03-19 17:25:54 +0100

    impossible d'acceder au compte si non connecté

--- Register/common/account.php
+++ Register/common/account.php
@@ -1,32 +1,30 @@
 <?php
-session_start();
-include_once '../inc/function.php'; ?>
-<?php include_once '../inc/header.php'; ?>
 
-<?php // if(isset($_SESSION['auth'])): ?>
+include_once '../inc/function.php';
+include_once '../inc/header.php'; ?>
+
+<?php if(isset($_SESSION['auth'])): ?>
 
 <h2>Votre compte</h2>
-<?php debug($_SESSION); ?>
 
-<?php //else: ?>
 <?php
     include_once '../inc/db.php';
     $query = $pdo->query("SELECT * FROM users");
     $users = $query->fetchAll(PDO::FETCH_OBJ);
-
+    $username = $_SESSION["auth"]->username;
 ?>
-<pre>
-<?php  print_r($users[0]->username);
- ?>
-</pre>
-<pre>
-<?php  echo ($users[0]->admin);
-?>
-</pre>
 
-<?php //endif; ?>
+
+<pre>
+<h3>Hello <?= $username ?>, how are you ? <br> your email is <?= $_SESSION["auth"]->email ?></h3>
+
 <div>
     <form action="modify.php">
      <button class="btn btn-primary">Modifier le compte</button>
     </form>
+</div>
 <?php include_once '../inc/footer.php'; ?>
+
+    <?php else: ?>
+    <?php header("location: login.php");?>
+    <?php endif; ?>
