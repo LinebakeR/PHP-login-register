6cc9285fd951d110b9e780cc71dd880b537d975e
alex <alexandre.gracias@coding-academy.fr>
2019-03-19 15:12:47 +0100

    recupmdpMAJ

--- Register/common/recupmdp.php
+++ Register/common/recupmdp.php
@@ -5,7 +5,6 @@ require_once "../class/form.php";
 if($_POST){
     echo "coucou ";
     if(!empty($_POST['email'])){
-        echo"coucou2 ";
         if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
             
             $email = $_POST['email'];
@@ -14,33 +13,31 @@ if($_POST){
             include_once "../inc/db.php";
             $confMail = $pdo->prepare('SELECT * FROM users WHERE email = ?');
             $confMail->execute([$email]);
-            $mailverif = $confMail->fetch();
-            echo"coucou 3"; 
-/* 
-            //2eme requete, ecrase ancien password dans BDD
-              */
+            $mailverif = $confMail->fetch(); 
+            
             
             //si mail OK, creation nouveau hash mdp
             if($mailverif){
-            
+                
                 echo "Email valide";
                 $recupCode = str_random(8);
                 $hash_code = password_hash($recupCode, PASSWORD_DEFAULT);
                 $newMdp = $pdo->prepare("UPDATE users SET password = '$hash_code' WHERE email = '$email'");
                 $newMdp->execute([$hash_code]);
+                mail($email, "Nouveau mot de passe", "Voici votre nouveau mot de passe " . $recupCode .);
                 //die(var_dump($recupCode)); pour test
                 
             }
             else{
-                echo "mail non existant";
+                echo "Cette adresse email n'est pas enregistré";
             }
         }
         else{
-            echo "mail non valide";
+            echo "L'email n'est pas valide";
         }
     }
     else{
-        echo "mail non entré";
+        echo "Veuillez entrer votre adresse mail";
     }
 }
 ?>
